b6f18d7277f5642b97231499c8dc052703bbf57c
Ana Barrios <abarrios.chavez@gmail.com>
2017-06-13 15:01:58 -0600

    RubySchool Class 2 - Ruby Fundamentals

---
+++ examples/arrays.rb
@@ -0,0 +1,15 @@
+puts 'Crea un arreglo e imprime, después agrega un elemento y vuelve a imprimir,
+      despues imprime el ultimo elemento de tu arreglo.'
+
+# Declara tu primer arreglo aqui
+
+puts "Arreglo original"
+puts nombre_de_tu_arreglo
+
+# Agrega un dato a tu arreglo
+
+puts "Arreglo con nuevo elemento"
+puts nombre_de_tu_arreglo
+
+puts "Ultimo elemento del arreglo"
+puts ultimo_elemento_del_arreglo
---
+++ examples/arrays_iteracion.rb
@@ -0,0 +1,15 @@
+puts "Utiliza el metodo each para sacar el promedio de las calificaciones"
+# HINT: Declara una variable antes, para sumar ahí el valor de cada iteración.
+
+calificaciones = [10, 8, 9, 5, 8, 10, 8]
+
+
+
+
+
+
+
+
+# HINT: Existe un metodo para saber el tamaño de un objeto, buscalo!
+# El promedio debe ser similar a 8.28
+puts promedio
