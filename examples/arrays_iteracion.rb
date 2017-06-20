90470371d76d12cbbb0ca8edb69079d707e649ee
rgagit <rgaruby@gmail.com>
2017-06-19 23:06:41 -0600

    Ejemplo2 Funcionando

--- examples/arrays_iteracion.rb
+++ examples/arrays_iteracion.rb
@@ -2,13 +2,17 @@ puts "Utiliza el metodo each para sacar el promedio de las calificaciones"
 # HINT: Declara una variable antes, para sumar ahí el valor de cada iteración.
 
 calificaciones = [10, 8, 9, 5, 8, 10, 8]
+longitud=calificaciones.length
+#puts longitud
 
+sumatoria = 0.00
+promedio = 0.00
 
-
-
-
-
-
+calificaciones.each do |calif|
+    sumatoria = sumatoria + calif.to_i
+end
+promedio =sumatoria/longitud
+puts 'El promedio es= '
 
 # HINT: Existe un metodo para saber el tamaño de un objeto, buscalo!
 # El promedio debe ser similar a 8.28, tiene que llevar decimales.
